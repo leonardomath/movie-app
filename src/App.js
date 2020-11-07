753b69306478476f01663bad9057ae9c48ec36b2
Leonardo Matheus <lleomath@gmail.com>
2020-11-07 03:11:52 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { parseISO, isDate } from 'date-fns'
+import Bulma from '../node_modules/bulma/css/bulma.min.css'
+import './App.css'
 
 function App() {
+
+  const [movieTitle, setMovieTitle] = React.useState(null)
+  const [Released, setReleased] = React.useState(null)
+  const [posterUrl, setPosterUrl] = React.useState(null)
+  const [name, setName] = React.useState(null)
+  const [error, setError] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+
+  async function searchMovie() {
+    setLoading(true)
+    setError(false)
+    const response = await fetch(`http://www.omdbapi.com/?t=${name}&apikey=ec9c2bae`)
+    const data = await response.json()
+    if (data.Error) {
+      setError(true)
+    } else {
+      setMovieTitle(data.Title)
+      setPosterUrl(data.Poster)
+      setReleased(data.Released)
+    }
+
+    setLoading(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="box-movie">
+          <h1 class="title">Digite o nome do filme</h1>
+          <input class={`input ${loading ? 'is-loading' : ''}`} type="text" placeholder="Text input" onChange={e => setName(e.target.value)} value={name}></input>
+          <button onClick={searchMovie} class="button is-link">Encontrar</button>
+        </div>
+        { loading ? <div class="loading"><button class="button is-loading">Loading</button></div> : ''}
+        { error ? 
+        <div class="notification is-danger">
+          Filme não encontrado.
+        </div> 
+        : 
+        <div className="movie">
+          <h1>{movieTitle}</h1>
+          <p>{Released}</p>
+          <img src={posterUrl} alt={movieTitle}/>
+        </div>  }
+        
+      </div>
     </div>
   );
 }
